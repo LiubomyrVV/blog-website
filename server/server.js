1b803d9870db5760f74133d8852a437af503e802
Liubomyr Vernyk <liubomyrpoland@gmail.com>
2024-05-15 22:07:52 +0200

    feat: Add deploy configuration

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require('cors');
 const nodemailer = require("nodemailer");
 
-
 const { port } = require("./constant");
 
 const getStoriesList = require("./getStoriesList")
