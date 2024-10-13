85b32768ab8a37798bb97800e8901835876256b6
liubomyr <liubomyrpoland@gmail.com>
2024-10-14 00:16:42 +0200

    Linked preloader to loaded data and fixed problem with sizes on 1680px extension

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import './styles/App.css';
 import React, { useEffect, useState } from 'react';
 import { screen } from './functions/Screen';
 import { Preloader } from './components/preloader/Preloader';
+import { getStories } from './service/getStories';
 
 
 
@@ -19,9 +20,21 @@ function App() {
 
   useEffect(() => {
     setScreenLoading(true);
-    setTimeout(() => {
-      setScreenLoading(false);
-    }, 2800);
+    getStories({ count: 1 })
+      .then((response) => {
+       try {
+          console.log(response)
+          setScreenLoading(false);;
+        } catch (err) {
+          throw new Error("Preloader error: ", err);
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+        setTimeout(() => {
+          setScreenLoading(false);
+        }, 2800);
+      });
   }, []);
 
   useEffect(() => {
